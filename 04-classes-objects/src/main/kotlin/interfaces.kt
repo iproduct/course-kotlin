81e511e95b7ab32c693bd7ec3d260e9f1b080f24
Trayan Iliev <office@iproduct.org>
2021-10-15 13:46:13 +0300

    examples added

---
+++ 04-classes-objects/src/main/kotlin/interfaces.kt
@@ -0,0 +1,78 @@
+package course.kotlin.interfaces
+
+import course.kotlin.Person3
+
+interface MyInterface {
+    fun bar()
+    fun foo() {
+        // optional body
+    }
+}
+
+class Child : MyInterface {
+    override fun bar() {
+        // body
+    }
+}
+
+interface MyInterface2 {
+    val prop: Int // abstract
+
+    val propertyWithImplementation: String
+        get() = "foo"
+
+    fun foo() {
+        print(prop)
+    }
+}
+
+class Child2 : MyInterface2 {
+    override val prop: Int = 29
+}
+
+interface Named {
+    val name: String
+}
+
+// Interface inheritance
+interface Person : Named {
+    val firstName: String
+    val lastName: String
+
+    override val name: String get() = "$firstName $lastName"
+}
+
+data class Position(val name: String)
+
+data class Employee(
+    // implementing 'name' is not required
+    override val firstName: String,
+    override val lastName: String,
+    val position: Position
+) : Person
+
+// conflict resolution
+interface A {
+    fun foo() { print("A") }
+    fun bar()
+}
+
+interface B {
+    fun foo() { print("B") }
+    fun bar() { print("bar") }
+}
+
+class C : A {
+    override fun bar() { print("bar") }
+}
+
+class D : A, B {
+    override fun foo() {
+        super<A>.foo()
+        super<B>.foo()
+    }
+
+    override fun bar() {
+        super<B>.bar()
+    }
+}
