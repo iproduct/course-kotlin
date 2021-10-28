984b266c7d8d737ee012598132b307bf80cc4f06
Trayan Iliev <office@iproduct.org>
2021-10-28 13:44:17 +0300

    presentation update

--- 04-classes-objects/src/main/kotlin/generics.kt
+++ 04-classes-objects/src/main/kotlin/generics.kt
@@ -18,14 +18,16 @@ fun demo(strs: Source<String>) {
     // ...
 }
 
-interface Comparable<in T> {
+interface Comparable2<in T> {
     operator fun compareTo(other: T): Int
 }
 
-fun demo(x: Comparable<Number>) {
+fun demo(x: Comparable2<Number>) {
     x.compareTo(1.0) // 1.0 has type Double, which is a subtype of Number
     // Thus, you can assign x to a variable of type Comparable<Double>
-    val y: Comparable<Double> = x // OK!
+    var y: Comparable2<Number> = x // OK!
+//    val z = 1
+//    y  = z
 }
 
 // type projection
@@ -46,11 +48,37 @@ fun copy(from: Array<out Any>, to: Array<Any>) {
         to[i] = from[i]
     } }
 //fun fill(dest: Array<in String>, value: String) { ... }
+
+// generic functions
+fun <T> singletonList(item: T): List<T> {
+    return emptyList()
+}
+fun <T> T.basicToString(): String { // extension function
+    return ""
+}
+
+// generic constraints
+fun <T : Comparable<T>> sort(list: List<T>) {  /*...*/ }
+fun <T> copyWhenGreater(list: List<T>, threshold: T): List<String>
+        where T : CharSequence,
+              T : Comparable<T> {
+    return list.filter { it > threshold }.map { it.toString() }
+}
+
 fun main() {
     val ints: Array<Int> = arrayOf(1, 2, 3)
     val any = Array<Any>(3) { "" }
     copy(ints, any)
     //   ^ type is Array<Int> but Array<Any> was expected
+
+    // generic functions
+    val l = singletonList<Int>(1)
+    val l2 = singletonList(1)
+
+    // generic constraints
+    sort(listOf(1, 2, 3)) // OK. Int is a subtype of Comparable<Int>
+//    sort(listOf(HashMap<Int, String>())) // Error: HashMap<Int, String> is not a subtype of Comparable<HashMap<Int, String>>
 }
+
 
 
