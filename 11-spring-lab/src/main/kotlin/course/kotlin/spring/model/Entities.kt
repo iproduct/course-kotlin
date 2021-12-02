51f5d5d58d7ca3692fedecd5c51f59b868c32751
Trayan Iliev <office@iproduct.org>
2021-12-02 14:20:23 +0200

    11-spring-lab extensions added

--- 11-spring-lab/src/main/kotlin/course/kotlin/spring/model/Entities.kt
+++ 11-spring-lab/src/main/kotlin/course/kotlin/spring/model/Entities.kt
@@ -1,12 +1,14 @@
 package course.kotlin.spring.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import course.kotlin.spring.extensions.toSlug
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.ManyToOne
+import javax.print.DocFlavor
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
@@ -17,7 +19,7 @@ class Blog(
     @NotNull @Size(min = 2, max = 60) var title: String,
     @NotNull @Size(min = 10, max = 2048) var content: String,
     @ManyToOne var author: User,
-//    var slug: String = title.toSlug(),
+    var slug: String = title.toSlug(),
     var pictiureUrl: String? = null,
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var created: LocalDateTime = LocalDateTime.now(),
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var modified: LocalDateTime = LocalDateTime.now()
@@ -31,7 +33,8 @@ class User(
     @NotNull @Size(min = 2, max = 30) var username: String,
     @NotNull @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,}$")
     var password: String,
-
+    @NotNull var role: Role  = Role.READER,
+    var active: Boolean = true,
     var pictiureUrl: String? = null,
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var created: LocalDateTime = LocalDateTime.now(),
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var modified: LocalDateTime = LocalDateTime.now()
