d57fa9115f126b123d9862467cf9d5bde746551c
Trayan Iliev <office@iproduct.org>
2021-12-02 14:12:08 +0200

    11-spring-lab entities added

---
+++ 11-spring-lab/src/main/kotlin/course/kotlin/spring/model/Entities.kt
@@ -0,0 +1,38 @@
+package course.kotlin.spring.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+
+@Entity
+class Blog(
+    @Id @GeneratedValue var id: Long? = null,
+    @NotNull @Size(min = 2, max = 60) var title: String,
+    @NotNull @Size(min = 10, max = 2048) var content: String,
+    @ManyToOne var author: User,
+//    var slug: String = title.toSlug(),
+    var pictiureUrl: String? = null,
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var created: LocalDateTime = LocalDateTime.now(),
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var modified: LocalDateTime = LocalDateTime.now()
+)
+
+@Entity
+class User(
+    @Id @GeneratedValue var id: Long? = null,
+    @NotNull @Size(min = 2, max = 40) var firstName: String,
+    @NotNull @Size(min = 2, max = 40) var lastName: String,
+    @NotNull @Size(min = 2, max = 30) var username: String,
+    @NotNull @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,}$")
+    var password: String,
+
+    var pictiureUrl: String? = null,
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var created: LocalDateTime = LocalDateTime.now(),
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) var modified: LocalDateTime = LocalDateTime.now()
+)
