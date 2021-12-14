961ef53ea724cc7f1d641a1eebb027c9b88e93af
Trayan Iliev <office@iproduct.org>
2021-12-15 00:37:45 +0200

    mockMvc test using Kotlin DSL added

--- 12-restmvc-lab2/src/main/kotlin/course/kotlin/spring/model/Dtos.kt
+++ 12-restmvc-lab2/src/main/kotlin/course/kotlin/spring/model/Dtos.kt
@@ -39,9 +39,9 @@ fun BlogCreateView.toBlogReflection() = with(::Blog) {
 
 data class BlogDetailsView(
     val id: Long,
-    @NotNull @Size(min = 2, max = 60) val title: String,
-    @NotNull @Size(min = 10, max = 2048) val content: String,
-    @ManyToOne val author: UserDetailsView? = null,
+    val title: String,
+    val content: String,
+    val author: UserDetailsView? = null,
     val slug: String = title.toSlug(),
     val pictureUrl: String? = null,
     val created: String,
@@ -62,16 +62,16 @@ fun Blog.toBlogDetailsView() = with(::BlogDetailsView) {
 
 // User Dtos
 data class UserCreateView(
-    @NotNull @Size(min = 2, max = 40) val firstName: String,
-    @NotNull @Size(min = 2, max = 40) val lastName: String,
-    @NotNull @Size(min = 2, max = 30) val username: String,
-    @NotNull @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,}$")
+    @field:NotNull @field:Size(min = 2, max = 40) val firstName: String,
+    @field:NotNull @field:Size(min = 2, max = 40) val lastName: String,
+    @field:NotNull @field:Size(min = 2, max = 30) val username: String,
+    @field:NotNull @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,}$")
     val password: String,
-    @NotNull val role: Role = Role.READER,
+    @field:NotNull val role: Role = Role.READER,
     val active: Boolean = true,
     val pictureUrl: String? = null,
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) val created: LocalDateTime = LocalDateTime.now(),
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) val modified: LocalDateTime = LocalDateTime.now(),
+    val created: LocalDateTime = LocalDateTime.now(),
+    val modified: LocalDateTime = LocalDateTime.now(),
     val id: Long? = null,
 )
 
@@ -81,8 +81,8 @@ data class UserDetailsView(
     val role: Role = Role.READER,
     val active: Boolean = true,
     val pictureUrl: String? = null,
-    val created: LocalDateTime = LocalDateTime.now(),
-    val modified: LocalDateTime = LocalDateTime.now(),
+    val created: String,
+    val modified: String,
     val name: String,
 ) {
 //    val name: String
@@ -102,8 +102,10 @@ fun UserCreateView.toUser() = User(
 
 fun User.toUserDetailsView() = with(::UserDetailsView) {
     val propertiesByName = User::class.memberProperties.associateBy { it.name }
-    callBy(parameters.associate { parameter ->
-        parameter to when (parameter.name) {
+    callBy(parameters.associateWith { parameter ->
+        when (parameter.name) {
+            UserDetailsView::created.name -> created.format()
+            UserDetailsView::modified.name -> modified.format()
             UserDetailsView::name.name -> "$firstName $lastName"
             else -> propertiesByName[parameter.name]?.get(this@toUserDetailsView)
         }
--- 12-restmvc-lab2/src/test/kotlin/course/kotlin/spring/web/RestControllerTestWebMvc.kt
+++ 12-restmvc-lab2/src/test/kotlin/course/kotlin/spring/web/RestControllerTestWebMvc.kt
@@ -1,17 +1,19 @@
 package course.kotlin.spring.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import course.kotlin.spring.Application
 import course.kotlin.spring.dao.BlogsRepository
 import course.kotlin.spring.dao.UsersRepository
 import course.kotlin.spring.model.Blog
-import course.kotlin.spring.model.Role
 import course.kotlin.spring.model.User
+import course.kotlin.spring.model.toBlogDetailsView
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.verify
+import org.hamcrest.CoreMatchers.equalToObject
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
-import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,7 +21,7 @@ import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MockMvcResultMatchersDsl
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
@@ -33,6 +35,9 @@ class RestControllerTestWebMvc(@Autowired val mockMvc: MockMvc) {
 
     @MockkBean
     private lateinit var usersRepository: UsersRepository
+
+    @Autowired
+    private lateinit var mapper: ObjectMapper
 
     @Test
     @WithMockUser(roles = ["ADMIN"])
@@ -51,33 +56,46 @@ class RestControllerTestWebMvc(@Autowired val mockMvc: MockMvc) {
             .andExpect(MockMvcResultMatchers.jsonPath("\$.[0].slug").value(spring5Blog.slug))
             .andExpect(MockMvcResultMatchers.jsonPath("\$.[1].author.username").value(juergen.username))
             .andExpect(MockMvcResultMatchers.jsonPath("\$.[1].slug").value(spring43Blog.slug))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$").isArray())
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.length()").value(2))
+            .andExpect(MockMvcResultMatchers.content().string(
+               mapper.writeValueAsString(listOf(spring5Blog.toBlogDetailsView(),spring43Blog.toBlogDetailsView()))))
 
         // verify repo method called
         verify { blogsRepository.findAll() }
         confirmVerified(blogsRepository)  // no other methods were called
     }
 
-//    @Test
-//    @WithMockUser(roles = ["ADMIN"])
-//    fun `List blogs`() {
-//        // setup
-//        val juergen = User("springjurgen", "Juergen", "Hoeller", "juergen1234&", id = 1)
-//        val spring5Blog = Blog("Spring Framework 5.0 goes GA", "Dear Spring community ...", juergen, id = 1)
-//        val spring43Blog = Blog("Spring Framework 4.3 goes GA", "Dear Spring community ...", juergen, id = 2)
-//        every { blogsRepository.findAll() } returns listOf(spring5Blog, spring43Blog)
-//
-//        //test
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/blogs").accept(MediaType.APPLICATION_JSON))
-//            .andExpect(MockMvcResultMatchers.status().isOk)
-//            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(MockMvcResultMatchers.jsonPath("\$.[0].author.username").value(juergen.username))
-//            .andExpect(MockMvcResultMatchers.jsonPath("\$.[0].slug").value(spring5Blog.slug))
-//            .andExpect(MockMvcResultMatchers.jsonPath("\$.[1].author.username").value(juergen.username))
-//            .andExpect(MockMvcResultMatchers.jsonPath("\$.[1].slug").value(spring43Blog.slug))
-//
-//        // verify repo method called
-//        verify { blogsRepository.findAll() }
-//        confirmVerified(blogsRepository)  // no other methods were called
-//    }
+    @Test
+    @WithMockUser(roles = ["ADMIN"])
+    fun `List blogs - Kotlin DSL`() {
+        // setup
+        val juergen = User("Juergen", "Hoeller", "springjurgen", "juergen1234&", id = 1)
+        val spring5Blog = Blog("Spring Framework 5.0 goes GA", "Dear Spring community ...", juergen, id = 1)
+        val spring43Blog = Blog("Spring Framework 4.3 goes GA", "Dear Spring community ...", juergen, id = 2)
+        every { blogsRepository.findAll() } returns listOf(spring5Blog, spring43Blog)
 
+        //test
+        mockMvc.get("/api/blogs") {
+            accept = MediaType.APPLICATION_JSON
+        }
+//            .andDo { print() }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("\$.[0].author.username") { value(juergen.username) }
+                jsonPath("\$.[0].slug") { value(spring5Blog.slug) }
+                jsonPath("\$.[1].author.username") { value(juergen.username) }
+                jsonPath("\$.[1].slug") { value(spring43Blog.slug) }
+                jsonPath("\$") { isArray() }
+                jsonPath("\$.length()") { equalTo(2) }
+                content {
+                    json(mapper.writeValueAsString(listOf(spring5Blog.toBlogDetailsView(),spring43Blog.toBlogDetailsView())))
+                }
+            }
+        // verify repo method called
+        verify { blogsRepository.findAll() }
+        confirmVerified(blogsRepository)  // no other methods were called
+
+    }
 }
