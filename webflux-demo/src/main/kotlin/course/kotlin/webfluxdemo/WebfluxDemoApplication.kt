ea3d86774af803a13bcf71de9bd3f511ad2cef12
Trayan Iliev <office@iproduct.org>
2021-12-16 14:45:26 +0200

    lab project added

---
+++ webflux-demo/src/main/kotlin/course/kotlin/webfluxdemo/WebfluxDemoApplication.kt
@@ -0,0 +1,11 @@
+package course.kotlin.webfluxdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxDemoApplication>(*args)
+}
---
+++ webflux-demo/src/test/kotlin/course/kotlin/webfluxdemo/WebfluxDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package course.kotlin.webfluxdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
